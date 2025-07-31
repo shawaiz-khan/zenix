cfa621c4c8ad5ba10c519b3a719419bf7fb9cc8e
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-07-31 12:19:50 +0500

    add: Route doc added for clarity

--- client/src/components/navbar/data.ts
+++ client/src/components/navbar/data.ts
@@ -14,18 +14,11 @@ export const navLinks: INavLinks[] = [
         label: "Home",
     },
     {
+        link: "/features",
+        label: "Features",
+    },
+    {
         link: "/about",
         label: "About",
-        children: [
-            {
-                link: "/about/1",
-                label: "About1",
-                description: "About1About1About1About1About1About1A bout1About1About1About1About1About1 About1About1About1About1Abou t1About1About1Ab out1About1 About1About1About1About1About1About1A bout1About1About1About1About1About1 About1About1About1About1Abou t1About1About1Ab out1About1",
-            },
-            {
-                link: "/about/2",
-                label: "About2",
-            },
-        ]
     },
 ];
