ad4e191a0410038f3dcb25c1f2c87eac43a15b8a
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-08-09 00:47:52 +0500

    fix: Route fixed

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { RegisterUser } from "../controllers/index.js";
+import { LoginUser, RegisterUser } from "../controllers/index.js";
 
 const router = express.Router();
 
 router.post("/register", RegisterUser);
-router.post("/login", () => console.log("Login route"));
+router.post("/login", LoginUser);
 router.post("/logout", () => console.log("Logout route"));
 router.post("/refresh", () => console.log("Refresh route"));
 
