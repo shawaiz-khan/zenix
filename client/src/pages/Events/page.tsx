d736f75d01a44451f24f00541b23b3fa7e6f5855
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-08-24 00:15:49 +0500

    feat: Featured Events ui fixed

--- client/src/pages/Events/page.tsx
+++ client/src/pages/Events/page.tsx
@@ -1,17 +1,17 @@
-import { Button } from "@/components";
+import { Button, Input } from "@/components";
 import { eventsData } from "./data";
 
 const Events: React.FC = () => {
     return (
         <main className="bg-white min-h-screen">
-            <span className="bg-bg-light h-40 flex justify-center items-center gap-1 text-3xl font-bold">
-                Featured Events
-            </span>
-            <div className="p-5 flex flex-col gap-5">
-                <h1>Search bar will go here</h1>
+            <div className="bg-bg-light h-60 flex flex-col gap-5 justify-center items-center p-6">
+                <h1 className="text-3xl font-bold">Featured Events</h1>
+                <Input placeholder="Search for events..." />
+            </div>
 
+            <div className="px-5 py-10 flex flex-col gap-10 md:grid md:grid-cols-4">
                 {eventsData.map((event, index) => (
-                    <div key={index} className="relative bg-bg-light/50 rounded-md p-5 flex flex-col gap-4 justify-center items-start shadow">
+                    <div key={index} className="relative bg-white rounded-md p-5 flex flex-col gap-4 justify-center items-start shadow">
                         <div>
                             <h1 className="text-lg font-bold">{event.title}</h1>
                             <p className="text-xs text-muted-foreground">{event.description.length > 150 ? event.description.slice(0, 150) + "..." : event.description}</p>
@@ -25,7 +25,7 @@ const Events: React.FC = () => {
                             ))}
                         </div>
 
-                        <Button label="More Info." className="w-full" />
+                        <Button label="More Info." className="w-full cursor-pointer" />
 
                         <div className="flex justify-between items-center text-xs w-full">
                             <h3>{event.creator.charAt(0).toUpperCase() + event.creator.slice(1)}</h3>
